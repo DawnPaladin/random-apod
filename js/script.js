3a53b704a75180476946c0788f7a566790a9ec15
James Harris <computersherpa@gmail.com>
2015-09-26 21:45:06 -0500

    Get a random image from APOD

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ function getRandomDate() {
 	var today = new Date().getTime();
 	return new Date(start + Math.random() * (today - start));
 }
+
+function formatDate(date) {
+	return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+}
+
+function requestImage(dateObj) {
+	console.log("Requesting image for", formatDate(getRandomDate()));
+	jQuery.getJSON('https://api.nasa.gov/planetary/apod', {
+		date: formatDate(dateObj),
+		concept_tags: false,
+		api_key: 'GcueLfEVOwz40G8milIABX9hA3tpqzn2gvxW7mGe'
+	}, setImage);
+}
+
+function setImage(json) {
+	console.log(JSON.stringify(json));
+	console.log(json.url);
+	jQuery('.picture img').attr('src', json.url);
+}
